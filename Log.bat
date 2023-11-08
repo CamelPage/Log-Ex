3a9ab2695ca14f2c12ab951ce80d7d224efe2309
CamelPage <105890927+CamelPage@users.noreply.github.com>
2023-11-08 19:49:16 +0100

    Update Log.bat

--- Log.bat
+++ Log.bat
@@ -2,4 +2,3 @@ copy /v data\latest.log data\logs\
 TIMEOUT /T 2
 rename data\logs\latest.log %date%-%time:~0,2%-%time:~3,2%-%time:~6,2%.log
 del /f /q data\latest.log
-echo LogFormat=Log-Ex>data\latest.log
